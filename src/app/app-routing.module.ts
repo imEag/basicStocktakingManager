656cbf2f4bff12b046d0026e0dd647c6b387bd64
Emmanuel <emma145@hotmail.es>
2022-01-16 11:55:41 -0500

    app routing is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+import { Page404Component } from './page404/page404.component';
+
+const routes: Routes = [
+  {path: '', 
+  children: [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+]},
+  {path: 'stocktaking', component: TableComponent},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TableComponent } from './table/table.component';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    TableComponent
+    TableComponent,
+    HomeComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, FormsModule
   ],
-  providers: [],
+  providers: [AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
